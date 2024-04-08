744aa276e24fbc9771fddc0b6ba25c05b10a367c
Noah Sfez <sfz.noah@gmail.com>
2024-04-08 20:08:38 +0200

    correction download cv

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -30,38 +30,38 @@ export default {
     <div style="display: flex;">
       <img src="../assets/images/cv.png" class="cv-img" data-aos="zoom-out" data-aos-duration="1000" data-aos-offset="200" alt="" @click="toggleModal">
     </div>
-    <a href="../assets/CV-Noah-Sfez-Alternance.pdf" download="CV-Noah-Sfez-Alternance.pdf">
+    <a href="../../public/CV-Noah-Sfez.pdf" download="CV-Noah-Sfez.pdf">
 
       <button class="botao">
-  <svg
-    class="mysvg"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    height="24px"
-    width="24px"
-    >
-      <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-      <g
-        stroke-linejoin="round"
-        stroke-linecap="round"
-        id="SVGRepo_tracerCarrier"
-      ></g>
-      <g id="SVGRepo_iconCarrier">
-        <g id="Interface / Download">
-          <path
-            stroke-linejoin="round"
-            stroke-linecap="round"
-            stroke-width="2"
-            stroke="#f1f1f1"
-            d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12"
-            id="Vector"
-          ></path>
-        </g>
-      </g>
-    </svg>
-    <span class="texto">Download</span>
-  </button>
+        <svg
+          class="mysvg"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          height="24px"
+          width="24px"
+          >
+            <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+            <g
+              stroke-linejoin="round"
+              stroke-linecap="round"
+              id="SVGRepo_tracerCarrier"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <g id="Interface / Download">
+                <path
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke-width="2"
+                  stroke="#f1f1f1"
+                  d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12"
+                  id="Vector"
+                ></path>
+              </g>
+            </g>
+          </svg>
+          <span class="texto">Download</span>
+      </button>
     </a>
     
 
