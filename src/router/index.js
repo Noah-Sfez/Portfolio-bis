8e4adeb339b8cdb97fed48cec55b13722a66fe9a
Noah Sfez <sfz.noah@gmail.com>
2024-04-14 20:59:56 +0200

    Routage 404

--- src/router/index.js
+++ src/router/index.js
@@ -13,9 +13,6 @@ const router = createRouter({
     {
       path: '/experience',
       name: 'experience',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/ExperienceView.vue')
     },
     {
